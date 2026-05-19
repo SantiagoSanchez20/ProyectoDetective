1e3711adcd462251e643af0f821d89979548c83e
Your Name <your@email.com>
2026-05-19 15:20:49 -0500

    busqueda en el scorerecord y mostrar ranking

--- main.cpp
+++ main.cpp
@@ -37,3 +37,52 @@ ScoreRecord* abbInsertar(ScoreRecord* raiz,
     }
     return raiz;
 }
+
+// Buscar (requerimiento 22)
+ScoreRecord* abbBuscar(ScoreRecord* raiz, const std::string& nombre) {
+    if (raiz == nullptr) return nullptr;
+    if (nombre == raiz->getNombreDetective()) return raiz;
+    if (nombre  < raiz->getNombreDetective()) return abbBuscar(raiz->izquierda, nombre);
+    return abbBuscar(raiz->derecha, nombre);
+}
+
+// Recorrido en orden: menor puntaje primero (requerimiento 23)
+//  El ABB está ordenado por nombre, para ordenar por puntaje se mete en un vector y ordenamos, asi no se cambia el ABB
+void abbRecolectar(ScoreRecord* raiz,
+                   std::vector<ScoreRecord*>& vec) {
+    if (raiz == nullptr) return;
+    abbRecolectar(raiz->izquierda, vec);
+    vec.push_back(raiz);
+    abbRecolectar(raiz->derecha, vec);
+}
+
+void abbMostrarRanking(ScoreRecord* raiz) {
+    std::vector<ScoreRecord*> vec;
+    abbRecolectar(raiz, vec);
+
+    if (vec.empty()) {
+        std::cout << "  (sin partidas registradas aun)\n";
+        return;
+    }
+
+    // Ordenar por puntaje ascendente (mejor = menos movimientos)
+    for (size_t i = 0; i < vec.size(); ++i) {
+        for (size_t j = i + 1; j < vec.size(); ++j) {
+            if (vec[j]->getMejorPuntaje() < vec[i]->getMejorPuntaje()) {
+                std::swap(vec[i], vec[j]);
+            }
+        }
+    }
+
+    std::cout << "\n+------------------------------------+\n";
+    std::cout << "|   RANKING HISTORICO DE DETECTIVES  |\n";
+    std::cout << "+------------------------------------+\n";
+    int pos = 1;
+    for (auto* r : vec) {
+        std::cout << "  " << pos++ << ". "
+                  << r->getNombreDetective()
+                  << "  ->  " << r->getMejorPuntaje()
+                  << " movimientos\n";
+    }
+    std::cout << "+------------------------------------+\n\n";
+}
